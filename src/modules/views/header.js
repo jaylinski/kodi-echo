e80dfd27ca9add8c8a29129307c98c91b763a720
Jakob Linskeseder <jakob.linskeseder@gmail.com>
2018-09-21 22:37:48 +0200

    Use label if no title is found
    The Kodi API sometimes only returns a `label`,
    so we check for this value too.

--- src/modules/views/header.js
+++ src/modules/views/header.js
@@ -1,9 +1,13 @@
+function getTitle(store) {
+  return store.playing ? store.playing.label || store.playing.title : '';
+}
+
 export default (store) => `
 <div class="c-header">
   <div class="c-header--logo">
     <img src="/assets/icon-48.png" alt="Kodi Logo">
   </div>
-  <div class="c-header--playing" title="${store.playing ? store.playing.label : ''}">
-    ${store.playing ? store.playing.label || store.playing.title : ''}
+  <div class="c-header--playing" title="${getTitle(store)}">
+    ${getTitle(store)}
   </div>
 </div>`;
