58cc945d9f99753bdc39afb9e399f0d60aaa8c1f
Jakob Linskeseder <jakob.linskeseder@gmail.com>
2020-01-24 15:01:05 +0100

    Use manifest homepage url in footer template
    This string was duplicated before.

--- src/modules/views/footer.js
+++ src/modules/views/footer.js
@@ -3,7 +3,7 @@ import { html } from './../utils/dom.js';
 export default (store, i18n, manifest) => html`
   <footer class="c-footer">
     <div class="c-footer__name">
-      <a href="https://www.github.com/jaylinski/kodi-echo" target="_blank">${manifest.name}</a>
+      <a href="${manifest.homepage_url}" target="_blank">${manifest.name}</a>
       <span>&nbsp;&bull;&nbsp;</span>
       <a @click="${() => store.commit('route', 'options')}" href="#">${i18n.getMessage('footerOptions')}</a>
       <span>&nbsp;&bull;&nbsp;</span>
