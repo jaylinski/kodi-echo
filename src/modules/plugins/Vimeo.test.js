20b2bf35d27f92d3755bbb2242c046f8b9cd7e23
Jakob Linskeseder <jakob.linskeseder@gmail.com>
2021-01-31 19:50:29 +0100

    Support sharing of unlisted Vimeo videos
    Related to jaylinski/kodi-addon-vimeo/issues/43

--- src/modules/plugins/Vimeo.test.js
+++ src/modules/plugins/Vimeo.test.js
@@ -17,6 +17,14 @@ test('Generates a plugin path from a channel URL', async () => {
   expect(pluginPath).toBe('plugin://plugin.video.vimeo/play/?video_id=388640017');
 });
 
+test('Generates a plugin path from an unlisted video URL', async () => {
+  const vimeo = new Vimeo();
+  const url = new URL('https://vimeo.com/12345/6789');
+  const pluginPath = await vimeo.getPluginPath({ url });
+
+  expect(pluginPath).toBe('plugin://plugin.video.vimeo/play/?video_id=12345:6789');
+});
+
 test('Generates a plugin path from a URL with tracking params', async () => {
   const vimeo = new Vimeo();
   const url = new URL('https://vimeo.com/1234567?foo=bar&utm=vimeo');
